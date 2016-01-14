f0b945e55fc97b8ef86a0bdafbec3bd9d00bed55
Willem Meints <willem@mein-design.nl>
2016-01-14 21:34:28 +0100

    Remove misplaced controller file

--- Scripts/Controllers/HomeController.ts
+++
@@ -1,9 +0,0 @@
-module Frontend.Controllers {
-    export class HomeController {
-
-    }
-
-    HomeController.$inject = [];
-
-    angular.module('frontend').controller('HomeController', HomeController);
-}
